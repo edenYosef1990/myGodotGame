cae0063361ba5fb68dbee491c110ee478a2666f7
eden <edenyo@rafael.co.il>
2024-05-28 10:25:09 +0300

    fixed selection box

--- GameManager.gd
+++ GameManager.gd
@@ -9,51 +9,41 @@ enum ControlState { None, Selecting }
 
 var gameState = ControlState.None
 var startRectangle : Vector2 = Vector2.ZERO;
+var endRectangle : Vector2 = Vector2.ZERO;
 
-func calculateRec(start: Vector2, curr: Vector2):
-	var topLeftX = min(start.x,curr.x)
-	var topLeftY = min(start.y, curr.y)
-	var w = abs(start.x - curr.x)
-	var h = abs(start.y - curr.y)
-	var rect = Rect.new()
-	rect.topLeft = Vector2(topLeftX,topLeftY)
-	rect.w = w;
-	rect.h = h;
-	return rect;
-	pass
-
-@onready var selectionRectangle : Node2D = $SelectionRectangle
-
+@onready var selectionRectangle : ColorRect = $ColorRect
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 	
-func updateRect(rect: Rect):
-	selectionRectangle.position = rect.topLeft + Vector2(rect.w / 2, rect.h / 2)
-	selectionRectangle.scale.x = rect.w / 64;
-	selectionRectangle.scale.y = rect.h / 64;
+func updateRect():
+	
+	var topLeft = Vector2(min(startRectangle.x,endRectangle.x), min(startRectangle.y,endRectangle.y))
+	selectionRectangle.position = topLeft;
+	selectionRectangle.size = (endRectangle - startRectangle).abs();
+	#selectionRectangle.scale.x = rect.w / 64;
+	#selectionRectangle.scale.y = rect.h / 64;
 	pass
 
 func _input(event):
 	if(event is InputEventMouseButton):
 		if event.is_pressed():
 			gameState = ControlState.Selecting
-			var rectnagle = Rect.new()
-			rectnagle.topLeft = event.position
-			startRectangle = event.position
-			updateRect(rectnagle)
-			
+			startRectangle = event.global_position
+			endRectangle = event.global_position
+			updateRect()	
 			print("Selecting")
 		else:
 			gameState = ControlState.None
-			var rectnagle = Rect.new()
-			updateRect(rectnagle)
+			startRectangle = Vector2.ZERO
+			endRectangle = Vector2.ZERO
+			updateRect()
 			print("None")
 			
 	if(event is InputEventMouseMotion):
 		if gameState == ControlState.Selecting:
-			var rectangle = calculateRec(startRectangle, event.position)
-			updateRect(rectangle)
+			endRectangle = event.global_position;
+			updateRect()
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
