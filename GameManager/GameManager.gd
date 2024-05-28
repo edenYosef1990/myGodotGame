41f1c1304d4fded79a95141e7d2b78a279d809a2
eden <edenyo@rafael.co.il>
2024-05-28 11:27:12 +0300

    refactored files structure

--- GameManager/GameManager.gd
+++ GameManager/GameManager.gd
@@ -11,7 +11,12 @@ var gameState = ControlState.None
 var startRectangle : Vector2 = Vector2.ZERO;
 var endRectangle : Vector2 = Vector2.ZERO;
 
-@onready var selectionRectangle : ColorRect = $ColorRect
+func isInRectangle(start: Vector2, end: Vector2, point: Vector2):
+	var topLeft = Vector2(min(startRectangle.x,endRectangle.x), min(startRectangle.y,endRectangle.y))
+	var dimension = (endRectangle - startRectangle).abs();
+	return topLeft.x <= point.x and point.x <= (topLeft.x + dimension.x) and topLeft.y <= point.y and point.y <= (topLeft.y + dimension.y) 
+
+@onready var selectionRectangle : ColorRect = $SelectionRectangle
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -21,8 +26,6 @@ func updateRect():
 	var topLeft = Vector2(min(startRectangle.x,endRectangle.x), min(startRectangle.y,endRectangle.y))
 	selectionRectangle.position = topLeft;
 	selectionRectangle.size = (endRectangle - startRectangle).abs();
-	#selectionRectangle.scale.x = rect.w / 64;
-	#selectionRectangle.scale.y = rect.h / 64;
 	pass
 
 func _input(event):
@@ -32,13 +35,13 @@ func _input(event):
 			startRectangle = event.global_position
 			endRectangle = event.global_position
 			updateRect()	
-			print("Selecting")
 		else:
 			gameState = ControlState.None
+			deselectAll()
+			selectUnitsInRegion(startRectangle, endRectangle)
 			startRectangle = Vector2.ZERO
 			endRectangle = Vector2.ZERO
 			updateRect()
-			print("None")
 			
 	if(event is InputEventMouseMotion):
 		if gameState == ControlState.Selecting:
@@ -52,5 +55,22 @@ func _process(delta):
 	
 func doAction():
 	print("action done")
+
+func selectUnitsInRegion(start: Vector2, end: Vector2):
+	var units = get_tree().get_nodes_in_group("selectable")
+	if units != null:
+		for unit in units:
+			var unitInWorld = unit as Tank
+			if(isInRectangle(start,end,unitInWorld.global_position)):
+				unitInWorld.toggleDisplayHealthBar(true)
+				print("boom")
+	pass
+	
+func deselectAll():
+	var units = get_tree().get_nodes_in_group("selectable")
+	if units != null:
+		for unit in units:
+			var unitInWorld = unit as Tank
+			unitInWorld.toggleDisplayHealthBar(false)
 	
 	
